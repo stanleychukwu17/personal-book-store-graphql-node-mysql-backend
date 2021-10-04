ff5ee7978b367561381ec334578d8f56431fa4b2
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-10-04 15:23:33 +0100

    changes

--- parser/functions.js
+++ parser/functions.js
@@ -2,7 +2,7 @@ const dbPool = require('./db_connection'); // the mysql database connection pool
 
 const getAllBooks = async (bookId = 0) => {
     const rows = dbPool.query('SELECT * FROM books').then(rws => {
-        console.log(rws);
+        // console.log(rws);
     });
 
     return [
