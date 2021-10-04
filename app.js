22e64e07987e5b92be582615d2925bdeea03e756
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-10-04 11:24:23 +0100

    changes

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 require('dotenv').config(); // sets up your dotenv environment
 const express = require('express');
-const mongoose = require('mongoose');
 const app = express();
 
 
@@ -8,17 +7,19 @@ const {graphqlHTTP} = require('express-graphql');
 const schema = require('./schema/schema');
 
 
-//--start-- connection to the mongodb server
-// mongoose.set('useFindAndModify', false);
-// mongoose.set('useCreateIndex', true);
-mongoose.connect(process.env.DEVELOPMENT_DB_DSN, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
-    app.listen(8080, () => { console.log("Listening on port 8080"); });
-});
+//--start-- connection to the mysql database
+    const pool = mysql.createPool({
+        connectionLimit : 10,
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database : 'nodejs_beers'
+    })
 //--end--
 
-// app.listen(4000, () => {
-//     console.log('listening on port ', 4000)
-// })
+app.listen(4000, () => {
+    console.log('listening on port ', 4000)
+})
 
 app.use('/graphql', graphqlHTTP({schema, graphiql: true}))
 
